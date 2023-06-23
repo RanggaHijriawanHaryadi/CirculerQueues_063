9fa6e3eaebadfb7477429d6c447c74ecc6639985
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2023-06-23 19:17:01 +0700

    Remove jika element diposisi akhir dan  front  kembali = 0

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -51,5 +51,12 @@ public:
 			FRONT = -1;
 			REAR = -1;
 		}
+		else {
+			// Jika elememt yang dihapus berada di posisi terakhir array, kembali ke awal
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
 	}
 };
