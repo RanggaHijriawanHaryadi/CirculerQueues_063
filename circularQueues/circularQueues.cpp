43332ca1c7a1067bad9fb0c3f04e61b9d3990c62
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2023-06-23 20:30:59 +0700

    membuat pilihan menu (1-4)di cin ch pada

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -104,6 +104,16 @@ int main() {
 	char ch;
 
 	while (true) {
-		
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert opration" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. IDisplay values" << endl;
+			cout << "4. Exit" << endl;
+			cout << "Enter your choice (1-4): ";
+			cin >> ch;
+			cout << endl;
+			
+		}
 	}
 }
